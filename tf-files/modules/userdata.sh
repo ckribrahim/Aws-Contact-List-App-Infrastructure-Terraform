4142109fe7f1e4a260c1a3e795653c4286f698de
Ibrahim Cakir <117196945+ckribrahim@users.noreply.github.com>
2026-04-04 15:53:09 +0300

    Update userdata.sh

--- tf-files/modules/userdata.sh
+++ tf-files/modules/userdata.sh
@@ -1,10 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 yum update -y
 yum install python3 -y
+yum install python3-devel -y
+yum install gcc -y
+yum install git -y
 pip3 install flask
 pip3 install flask_mysql
+pip3 install pymysql
 pip3 install cryptography
-yum install git -y
 cd /home/ec2-user
 USER=${user-data-git-name}
 git clone https://github.com/$USER/Aws-Contact-List-App-Infrastructure-Terraform.git
